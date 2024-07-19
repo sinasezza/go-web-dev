2b7a628f694529a9f22292aa80d146b4e5d90d68
sinasezza <s.e.sezza121@gmail.com>
2024-07-19 17:09:54 +0330

    feat(models): postgresql config file and connection structure added to models package.

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/sinasezza/go-web-dev/models"
 )
 
 func main() {
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// setup the database
+	cfg := models.DefaultPostgresConfig()
+
+	fmt.Println("DATABASE CONNECTION INFO: ", cfg.String())
+
+	// create router
 	r := chi.NewRouter()
+
+	// global middlewares
 	r.Use(middleware.Logger)
+
+	// routes
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
---
+++ models/postgres.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func Open(config PostgresConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", config.String())
+	if err != nil {
+		return nil, fmt.Errorf("open: %w", err)
+	}
+	return db, nil
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Database: os.Getenv("POSTGRES_DATABASE"),
+		SSLMode:  "disable",
+	}
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
